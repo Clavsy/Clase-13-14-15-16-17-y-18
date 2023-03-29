a9514b66220a8f56562f1b50cd47e856a9ec270c
Joaquin Rego <clavsyofficial@gmail.com>
2023-03-29 18:59:16 -0300

    Ejercicios clase 13, 14, 15, 16, 17 y 18 en Egg

--- EjercicioPractico#4/src/ejerciciopractico/pkg4/Main.java
+++ EjercicioPractico#4/src/ejerciciopractico/pkg4/Main.java
@@ -5,6 +5,8 @@
  */
 package ejerciciopractico.pkg4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author clavs
@@ -15,7 +17,38 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Rectangulo figura1 = new Rectangulo();
+        
+        crearRectangulo(figura1);
+        System.out.println(" ");
+        calcularSuperficie(figura1);
+        calculaPerimetro(figura1);
+        System.out.println(" ");
+        graficarRectangulo(figura1);
+    }
+
+    private static void crearRectangulo(Rectangulo figura1) {
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese la base del rectangulo:");
+        figura1.setBase(leer.nextDouble());
+        System.out.println("Ingrese la altura del rectangulo:");
+        figura1.setAltura(leer.nextDouble());
+        
+    }
+
+    private static void calcularSuperficie(Rectangulo figura1) {
+        System.out.println("Superficie = " + figura1.calcularSuperficie());
+    }
+
+    private static void calculaPerimetro(Rectangulo figura1) {
+        System.out.println("Perimetro = " + figura1.calcularPerimetro());
+    }
+
+    private static void graficarRectangulo(Rectangulo figura1) {
+        figura1.graficarRectangulo();
     }
     
 }
--- EjercicioPractico#4/src/ejerciciopractico/pkg4/Rectangulo.java
+++ EjercicioPractico#4/src/ejerciciopractico/pkg4/Rectangulo.java
@@ -10,5 +10,55 @@ package ejerciciopractico.pkg4;
  * @author clavs
  */
 public class Rectangulo {
-    
+
+    private double base;
+    private double altura;
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public Rectangulo() {
+    }
+
+    public double calcularSuperficie() {
+        return base * altura;
+    }
+
+    public double calcularPerimetro() {
+        return (base + altura) * 2;
+    }
+
+    public void graficarRectangulo() {
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < base; j++) {
+                if (i == 0 || j == 0 || i == altura - 1 || j == base - 1) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println(" ");
+        }
+    }
 }
+
